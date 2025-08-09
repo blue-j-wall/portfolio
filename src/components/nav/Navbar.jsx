5d389630e93fada3e3f74ceda8629a99f0a2325d
G-Wall <gracewall@outlook.com>
2025-08-08 20:40:31 -0500

    offcanvas closes on selection

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -14,6 +14,11 @@ export default function SiteNavbar(props) {
         element.classList.toggle("dark-mode");
     }
 
+    const [showOffcanvas, setShowOffcanvas] = useState(false);
+    const handleCloseOffcanvas = () => setShowOffcanvas(false);
+    const handleShowOffcanvas = () => setShowOffcanvas(true);
+    
+
     return <Navbar id="portfolio-navbar" fixed="top" expand="md" collapseOnSelect>
         <Container id="nav-container" >
             <Navbar.Brand smooth as={HashLink} to="/#top">
@@ -26,35 +31,32 @@ export default function SiteNavbar(props) {
                 />{' '}
             </Navbar.Brand>
 
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" className="ms-auto" />
+            <Navbar.Toggle id="toggle" aria-controls="responsive-navbar-nav" className="ms-auto" onClick={handleShowOffcanvas} />
 
-            {/*<Navbar.Collapse id="responsive-navbar-nav" > menu drops from navbar*/}
-            <Navbar.Offcanvas id={`offcanvasNavbar`} aria-labelledby={`offcanvasNavbarLabel`} placement="end" >
-            <Offcanvas.Header closeButton> <Offcanvas.Title></Offcanvas.Title> </Offcanvas.Header>
-            <Offcanvas.Body>
-            
-                <Nav id="tabs" className="ms-auto justify-content-center">
-                    <Nav.Link as={HashLink} smooth to='/#about' style={{textDecoration:"none"}}>About</Nav.Link>
-                    <Nav.Link as={Link} to="/programming">Projects</Nav.Link>
-                    <Nav.Link as={Link} to="/linguistics">Research</Nav.Link>
-                    {/*<Nav.Link as={Link} to="https://www.linkedin.com/" target="_blank"><FaLinkedin/></Nav.Link>
-                    <Nav.Link as={Link} to="https://github.com/G-Wall" target="_blank"><FaGithub/></Nav.Link>
-                    <Nav.Link as={Link} to="mailto:jamiewall247@gmail.com" target="_blank"><FaEnvelope/></Nav.Link>*/}
-                </Nav>
+            <Navbar.Offcanvas id="offcanvasNavbar" placement="end" show={showOffcanvas} onHide={handleCloseOffcanvas}>
+                <Offcanvas.Header closeButton> <Offcanvas.Title></Offcanvas.Title> </Offcanvas.Header>
+                <Offcanvas.Body>
+                
+                    <Nav id="tabs" className="ms-auto justify-content-center">
+                        <Nav.Link as={HashLink} smooth to='/#about' style={{textDecoration:"none"}} onClick={handleCloseOffcanvas}>About</Nav.Link>
+                        <Nav.Link as={Link} to="/programming" onClick={handleCloseOffcanvas}>Projects</Nav.Link>
+                        <Nav.Link as={Link} to="/linguistics" onClick={handleCloseOffcanvas}>Research</Nav.Link>
+                        {/*<Nav.Link as={Link} to="https://www.linkedin.com/" target="_blank"><FaLinkedin/></Nav.Link>
+                        <Nav.Link as={Link} to="https://github.com/G-Wall" target="_blank"><FaGithub/></Nav.Link>
+                        <Nav.Link as={Link} to="mailto:jamiewall247@gmail.com" target="_blank"><FaEnvelope/></Nav.Link>*/}
+                    </Nav>
 
-                <Nav className="d-flex justify-content-end" horizontal>
-                        <ListGroup id="icons" className="d-flex justify-content-start" horizontal>
-                            <ListGroup.Item ><a href="https://www.linkedin.com/" target="_blank"><FaLinkedin/></a></ListGroup.Item>
-                            <ListGroup.Item ><a href="https://github.com/G-Wall" target="_blank"><FaGithub/></a></ListGroup.Item>
-                            <ListGroup.Item ><a href="mailto:jamiewall247@gmail.com" target="_blank"><FaEnvelope/></a></ListGroup.Item>
-                        </ListGroup>
-                </Nav>
+                    <Nav className="d-flex justify-content-end" horizontal>
+                            <ListGroup id="icons" className="d-flex justify-content-start" horizontal>
+                                <ListGroup.Item ><a href="https://www.linkedin.com/" target="_blank" onClick={handleCloseOffcanvas}><FaLinkedin/></a></ListGroup.Item>
+                                <ListGroup.Item ><a href="https://github.com/G-Wall" target="_blank" onClick={handleCloseOffcanvas}><FaGithub/></a></ListGroup.Item>
+                                <ListGroup.Item ><a href="mailto:jamiewall247@gmail.com" target="_blank" onClick={handleCloseOffcanvas}><FaEnvelope/></a></ListGroup.Item>
+                            </ListGroup>
+                    </Nav>
 
-            </Offcanvas.Body>
-            
+                </Offcanvas.Body>
             </Navbar.Offcanvas>
-            {/*</Navbar.Collapse>*/}
-
+            
             <Navbar.Text>
                 <a onClick={handleColorMode} id="color-toggle" className="d-flex align-items-center">
                     {colorMode ? <FaSun/> : <FaMoon/>}
