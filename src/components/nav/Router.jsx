bc2630fa0123ed6b6b7d6f7e011bac737b757a85
blue-j-wall <gracewall@outlook.com>
2025-10-04 15:32:30 -0500

    added behavior: scroll to top upon navigation

--- src/components/nav/Router.jsx
+++ src/components/nav/Router.jsx
@@ -1,5 +1,7 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 
+import ScrollToTop from "../ScrollToTop";
+
 import Portfolio from "../Portfolio";
 import Home from "./pages/Home";
 import CompSci from "./pages/CompSci";
@@ -7,14 +9,16 @@ import Ling from "./pages/Ling";
 
 export default function Router() {
     return <HashRouter>
-        <Routes>
-            <Route path="/" element={<Portfolio />}>
-                <Route index element={<Home />} />
-                <Route path="home" element={<Home />} />
-                <Route path="projects" element={<CompSci />} />
-                <Route path="research" element={<Ling />} />
-                <Route path="*" element={<Home />} />
-            </Route>
-        </Routes>
+        <ScrollToTop>
+            <Routes>
+                <Route path="/" element={<Portfolio />}>
+                    <Route index element={<Home />} />
+                    <Route path="home" element={<Home />} />
+                    <Route path="projects" element={<CompSci />} />
+                    <Route path="research" element={<Ling />} />
+                    <Route path="*" element={<Home />} />
+                </Route>
+            </Routes>
+        </ScrollToTop>
     </HashRouter>
 }
